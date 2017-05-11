cf829a7d4d068d2fc3bb521287644c0103c787f9
Bret Hubbard <bhubbard@landmarkhw.com>
2017-05-11 10:11:44 -0600

    adding menu back in

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-// const Menu = electron.Menu;
+const Menu = electron.Menu;
 // electron.crashReporter.start();
 
 // メインウィンドウの参照をグローバルに持っておく。
 var mainWindow = null;
 
 // メニューの定義
-// var menu = Menu.buildFromTemplate([{
-//         label: 'File',
-//         submenu: [
-//             { label: 'Exit', click: onExit }
-//         ]
-//     },
-//     {
-//         label: 'View',
-//         submenu: [
-//             { label: 'simple flow', click: () => navigate('view/simple_flow.html') },
-//             { label: 'flow direction', click: () => navigate('view/flow_direction.html') },
-//             { label: 'grid placement sample', click: () => navigate('view/grid_placement.html') },
-//             { label: 'z-index sample', click: () => navigate('view/z_index.html') },
-//             { label: 'holy grail layout', click: () => navigate('view/holy_grail.html') },
-//             { label: 'grid-template-areas sample', click: () => navigate('view/grid_template_areas.html') },
-//             { label: 'responsive layout sample', click: () => navigate('view/responsive.html') },
-//             { type: "separator" },
-//             { label: 'Reload', accelerator: 'CmdOrCtrl+R', click: () => mainWindow.restart() },
-//             { label: 'Toggle Developper Tools', accelerator: 'CmdOrCtrl+Shift+I', click: () => mainWindow.toggleDevTools() },
-//         ]
-//     },
-// ]);
+var menu = Menu.buildFromTemplate([{
+        label: 'File',
+        submenu: [
+            { label: 'Exit', click: onExit }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            { label: 'simple flow', click: () => navigate('view/simple_flow.html') },
+            { label: 'flow direction', click: () => navigate('view/flow_direction.html') },
+            { label: 'grid placement sample', click: () => navigate('view/grid_placement.html') },
+            { label: 'z-index sample', click: () => navigate('view/z_index.html') },
+            { label: 'holy grail layout', click: () => navigate('view/holy_grail.html') },
+            { label: 'grid-template-areas sample', click: () => navigate('view/grid_template_areas.html') },
+            { label: 'responsive layout sample', click: () => navigate('view/responsive.html') },
+            { type: "separator" },
+            { label: 'Reload', accelerator: 'CmdOrCtrl+R', click: () => mainWindow.restart() },
+            { label: 'Toggle Developper Tools', accelerator: 'CmdOrCtrl+Shift+I', click: () => mainWindow.toggleDevTools() },
+        ]
+    },
+]);
 
 app.commandLine.appendSwitch("--enable-experimental-web-platform-features");
 
-// Menu.setApplicationMenu(menu);
+Menu.setApplicationMenu(menu);
 
 // すべてのウィンドウが閉じられた際の動作
 app.on('window-all-closed', function() {
